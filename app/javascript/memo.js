e16f02088edd61b4dae8dc689f9a7d5077284cb3
koluhei <qpal79@gmail.com>
2021-07-08 16:37:35 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,14 @@
+function post(){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XMR = new XMLHttpRequest();
+    XMR.open("POST", "/posts", true);
+    XMR.responseType = "json";
+    XMR.send(formData);
+  });
+};
+
+window.addEventListener("load", post);
